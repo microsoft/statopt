d002d3497e1733fc9477cb11d9c83b9e594155f2
Lin Xiao <lixiao@microsoft.com>
2020-03-27 17:09:47 -0700

    recreated dist file for pip package v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='statopt',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Statistical adaptive stochastic optimization methods',
